fc525504b08b372e5d6f47ed3e66763b6002dfa8
angmay <70976070+angmay@users.noreply.github.com>
2021-02-16 19:31:37 -0500

    rawdata update

--- actualApp/static/js/rawdata.js
+++ actualApp/static/js/rawdata.js
@@ -8,8 +8,6 @@ d3.json("/api/jobs").then( function(response) {
 
     var jobPost = response["jobs"];
     //console.log(jobPost);
-
-    var summary = {};
     
     //counting jobs by type of role (e.g. 1000 postings for job analyst roles)
     jobPost[0].forEach(function(post) {
@@ -28,3 +26,42 @@ d3.json("/api/jobs").then( function(response) {
 
 });
 
+function updateRawdata(countrySelected) {
+
+    //Clear table before rendering 
+    table.html(" ");
+
+    d3.json("/api/jobs").then( function(response) {
+
+        var jobPost = response["jobs"];
+        //console.log(jobPost);
+
+        //if countryselected is "ALL", default is to show results for all countries
+        if (countrySelected == "All") {
+            //console.log("country selected ALL!!");
+            var filteredList = jobPost[0]}
+        else { //otherwise filter for selected country
+            //console.log("country selected. filtering now!");
+            var filteredList = jobPost[0].filter(d => d[2] == countrySelected)}
+
+        
+        //counting jobs by type of role (e.g. 1000 postings for job analyst roles)
+        filteredList.forEach(function(post) {
+        
+            var row = table.append("tr");
+    
+            // Extracting data from each key value pair in the data dictionary
+        
+            Object.entries(report).forEach( function([key,value]) {
+                // Adding column 
+                var cell = row.append("th");
+                // Adding Value
+                cell.text(value);
+            });
+        });
+    
+           
+    });
+
+
+};
